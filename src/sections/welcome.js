40a4e8454e6d523fb1ccbb97d1e2d0b6a593c292
Harshav0423 <harsha.valiveti98@gmail.com>
2023-09-08 21:24:21 -0400

    changes in page responsiveness

--- src/sections/welcome.js
+++ src/sections/welcome.js
@@ -11,23 +11,23 @@ export default function Welcome() {
 						className="profile-picture"
 						src={profilePhoto}
 						alt="profile picture"
-						height='220'
-						width='220'
+						height="220"
+						width="220"
 					/>
 				</div>
 				<div className="welcome-content">
-					<p>
+					<p className="welcome-text">
 						Hi I'm Harsha Valiveti, a recent Computer Science graduate student
-						from IUB. I'm passionate about building innovative and user-friendly
-						software. I'm skilled in Java, Python and C++, and I have 2yrs+
-						experience in working as Software Engineer. I'm always eager to
-						learn new things. I'm currently looking for Full-Time roles in
-						Software Engineering, Machine Learning and Data Engineering, with
-						immediate join.
+						from IUB. I am a sensible and detail-oriented software engineer with
+						strong problem-solving and critical thinking skills. I'm skilled in
+						Java, Python and C++, and I have 2yrs+ experience in working as
+						Software Engineer. I'm always eager to learn new things. I'm
+						currently looking for Full-Time roles in Software Engineering,
+						Machine Learning and Data Engineering, with immediate join.
 					</p>
-					<p>
+					<p className="welcome-text">
 						In my spare time, I enjoy building fun projects, playing Badminton
-						and recently started reading books on sci-fi.
+						and recently started reading books on sci-fi(Dune).
 					</p>
 				</div>
 			</div>
